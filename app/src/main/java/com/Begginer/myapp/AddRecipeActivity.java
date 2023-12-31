c5448a68c189f3aeebd6b359831f80f02f43299b
yaelalfa <yaelalfa0@gmail.com>
2023-12-31 05:08:22 +0200

    added~ add recipe without functionality , notFound ingredient dialog

--- app/src/main/java/com/Begginer/myapp/AddRecipeActivity.java
+++ app/src/main/java/com/Begginer/myapp/AddRecipeActivity.java
@@ -2,13 +2,33 @@ package com.Begginer.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AddRecipeActivity extends AppCompatActivity {
+
+    private TextView notFound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+        notFound=(TextView) findViewById(R.id.notFoundI);
+        notFound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNotFoundIngredient();
+            }
+        });
+
+    }
+
+    private void addNotFoundIngredient(){
+        Dialog dialog=new Dialog(this);
+        dialog.setContentView(R.layout.not_found_dialog);
+        dialog.show();
+
     }
 }
