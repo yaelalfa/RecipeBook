0738fe01864bc37f0252d38cb02a4e3ae63c1c3c
yaelalfa <yaelalfa0@gmail.com>
2023-12-31 06:09:57 +0200

    update dialog

--- app/src/main/java/com/Begginer/myapp/AddRecipeActivity.java
+++ app/src/main/java/com/Begginer/myapp/AddRecipeActivity.java
@@ -2,14 +2,20 @@ package com.Begginer.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class AddRecipeActivity extends AppCompatActivity {
 
     private TextView notFound;
+    private String[] types={"יבשים","רטובים","פירות","ירקות"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +32,21 @@ public class AddRecipeActivity extends AppCompatActivity {
     }
 
     private void addNotFoundIngredient(){
-        Dialog dialog=new Dialog(this);
-        dialog.setContentView(R.layout.not_found_dialog);
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        LayoutInflater inf=getLayoutInflater();
+        View dialogView=inf.inflate(R.layout.not_found_dialog,null);
+        builder.setView(dialogView);
+
+        Spinner Dspinner=dialogView.findViewById(R.id.spinnerType);
+        ArrayAdapter<CharSequence> ad=ArrayAdapter.createFromResource(this,R.array.spinnerType, android.R.layout.simple_spinner_item);
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Dspinner.setAdapter(ad);
+        AlertDialog dialog=builder.create();
         dialog.show();
+
+
+
 
     }
 }
